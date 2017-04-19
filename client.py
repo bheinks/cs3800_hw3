58d49dc4bb5091b47a741afdd78a98c09ed735a5
Brett Heinkel <bheinks@gmail.com>
2017-04-18 19:19:17 -0500

    properly shutdown after disconnected from server

--- client.py
+++ client.py
@@ -2,8 +2,9 @@
 
 import sys
 import socket
-import select
-import threading
+
+from threading import Thread
+from select import select
  
 class ChatClient:
     def __init__(self, hostname, port, username):
@@ -18,39 +19,50 @@ class ChatClient:
             print("Unable to connect")
             sys.exit()
          
-        print("Connected to {}. You can start sending messages".format(hostname))
+        # send username to server
         self.server_socket.send(self.username.encode())
 
-        self.listen_thread = threading.Thread(target = self.listen)
+        self.listen_thread = Thread(target = self.listen)
         self.listen_thread.start()
 
     def listen(self):
-        listening = True
-
-        while listening:
+        while True:
             data = self.server_socket.recv(4096).decode()
 
             if not data:
                 print("Disconnected from chat server")
-                listening = False
+                self.reading = False
+                break
             else:
-                #print data
+                # print data
                 sys.stdout.write(data)
                 sys.stdout.write('> ')
                 sys.stdout.flush()     
-
-        sys.exit()
     
     def start(self):
-        while True:
-            sys.stdout.write('> ')
-            sys.stdout.flush() 
-            msg = sys.stdin.readline()
-            self.server_socket.send(msg.encode())
-            
-            if msg.strip() in ["/quit", "/part", "/exit"]:
-                self.listen_thread.join()
-                return
+        self.reading = True
+
+        sys.stdout.write('> ')
+        sys.stdout.flush() 
+
+        while self.reading:
+            ready_to_read, ready_to_write, error = select([sys.stdin], [], [], 1)
+
+            if ready_to_read:
+                sys.stdout.write('> ')
+                sys.stdout.flush() 
+
+                try:
+                    msg = sys.stdin.readline()
+                except KeyboardInterrupt:
+                    print("\nPlease type /exit, /quit or /part to exit the chatroom")
+                    continue
+
+                self.server_socket.send(msg.encode())
+                
+                if msg.strip() in ["/quit", "/part", "/exit"]:
+                    self.listen_thread.join()
+                    return
 
 if __name__ == "__main__":
     hostname, port = input("Hostname: ").split(':')
